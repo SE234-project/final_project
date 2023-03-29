91d060a261bb844c95a1a97334a27ecb71b13f0d
Suchanun <Suchanun_sir@cmu.ac.th>
2023-03-29 19:09:36 +0700

    convert html to ejs

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static('public'));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res) {
+    res.render('index');
 })
 
+
+
+app.get("/indexCustomer.ejs", function(req, res) {
+    res.render('indexCustomer');
+})
+app.get("/indexKitchen.ejs", function(req, res) {
+    res.render('indexKitchen');
+})
+app.get("/register.ejs", function(req, res) {
+    res.render('register');
+})
 app.listen("3000", () => {
     console.log("Server is running on Port 3000.");
-  });
+});
